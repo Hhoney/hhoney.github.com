7f19a3be2db55ffe352ffa48c228505f55d991d4
hanchuchu <hancc@asiainfo.com>
2017-01-06 22:08:55 +0800

    update code

--- photo/js/myPhoto.js
+++ photo/js/myPhoto.js
@@ -5,20 +5,34 @@ $(document).ready(function(){
     $('.showAttrPhoto div').css({'height':attrWidth+'px','line-height':attrWidth+'px'});
     let boxWidth = $('.boutiqueBox').width();
     $('.boutiqueBox').css('height',boxWidth/3 + 'px');
-    let onlyWidth = $('.onlyBox').width();
-    $('.onlyBox').css('height',onlyWidth/3 + 'px');
+    let onlyBoxWidth = $('.onlyBox').width();
+    $('.onlyBox').css('height',onlyBoxWidth/3 + 'px');
     let footMargin = $('.footer').width();
-    $('.footer').css('margin-top',footMargin/5 + 'px');
+    $('.footer').css('margin-top',footMargin/4 + 'px');
+    let onlyWidth = $('.only').width();
+    $('.only').css('margin-top',onlyWidth/4 + 'px');
+    setTimeout(function(){
+        let boxHeight = $('.boutiqueBox').height();
+        let imgHeight = $('.right').height();
+        $('.forever').css('line-height',(boxHeight - imgHeight) + 'px');
+    },100);
     $(window).resize(function() {
+        setTimeout(function(){
+            boxHeight = $('.boutiqueBox').height();
+            imgHeight = $('.right').height();
+            $('.forever').css('line-height',(boxHeight - imgHeight) + 'px');
+        },100);
+        boxWidth = $('.boutiqueBox').width();
+        footMargin = $('.footer').width();
         imgWidth = $('.showConPhoto div').width();
         attrWidth = $('.showAttrPhoto div').width();
-        onlyWidth = $('.onlyBox').width();
-        $('.onlyBox').css('height',onlyWidth/3 + 'px');
-        boxWidth = $('.boutiqueBox').width();
+        onlyBoxWidth = $('.onlyBox').width();
+        $('.onlyBox').css('height',onlyBoxWidth/3 + 'px');
         $('.boutiqueBox').css('height',boxWidth/3 + 'px');
-        footMargin = $('.footer').width();
-        $('.footer').css('margin-top',footMargin/5 + 'px');
+        $('.footer').css('margin-top',footMargin/4 + 'px');
         $('.showConPhoto div').css({'height':imgWidth+'px','line-height':imgWidth+'px'});
         $('.showAttrPhoto div').css({'height':attrWidth+'px','line-height':attrWidth+'px'});
+        onlyWidth = $('.only').width();
+        $('.only').css('margin-top',onlyWidth/4 + 'px');
     });
 });
